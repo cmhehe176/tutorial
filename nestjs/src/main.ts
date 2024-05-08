1aa337b09912419e53cda2dbf16db55e04c885b5
Cong Minh <ndmc.system176@gmail.com>
2024-05-08 11:47:05 +0700

    fix: space

--- nestjs/src/main.ts
+++ nestjs/src/main.ts
@@ -3,10 +3,12 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
+  
   const app = await NestFactory.create(AppModule);
 
   const config = app.get(ConfigService);
 
   await app.listen(config.get('PORT'));
 }
+
 bootstrap();
