63018f43335c8d0baea4ada4c70d023d81c5bea6
Cong Minh <ndmc.system176@gmail.com>
2024-05-08 14:19:53 +0700

    feat: datasource , database ,...

---
+++ nestjs/database/datasource.ts
@@ -0,0 +1,13 @@
+import 'dotenv/config';
+import { DataSource } from 'typeorm';
+
+export default new DataSource({
+	type: 'mysql',
+	host: '127.0.0.1',
+	port: +process.env.MYSQL_PORT,
+	username: process.env.MYSQL_USERNAME,
+	password: process.env.MYSQL_PASSWORD,
+	database: process.env.MYSQL_DATABASE,
+	entities: ['src/entities/*.entity.ts'],
+	migrations: ['database/migrations/*.ts'],
+});
---
+++ nestjs/src/database/database.module.ts
@@ -0,0 +1,35 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+// nên chia ra làm một module riêng để clear code hơn là so với nhét thằng typeorm trực tiếp vào app module
+
+@Module({
+	imports: [
+		TypeOrmModule.forRootAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+
+			// cách để kết nối động với typeorm
+
+			useFactory: (configService: ConfigService) => ({
+				type: 'postgres',
+				host: configService.get('database.'),
+				port: configService.get('database.'),
+				username: configService.get('database.'),
+				password: configService.get('database.'),
+				database: configService.get('database.'),
+				entities: [],
+				synchronize: false,
+				autoLoadEntities: true,
+				migrations: ['dist/database/migrations/*.js'],
+				migrationsRun: configService.get('NODE_ENV') !== 'development',
+
+				//ở đây khi đặt là true thì sẽ tự động thay đổi data mỗi khi có sự thay đổi đầu vào
+				//tại sao chỗ khác ngta hay đặt là false nhỉ ???? trong dự án thực tế con Oni ý
+			
+			}),
+		}),
+	],
+})
+export class DatabaseModule { }
