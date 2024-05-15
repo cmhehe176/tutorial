aeef0f6334fb5bda29bb8fa54b5e63e9ce561b64
Cong Minh <ndmc.system176@gmail.com>
2024-05-15 13:22:55 +0700

    change name module to feature

--- nestjs/src/app.module.ts
+++ nestjs/src/app.module.ts
@@ -3,10 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
-import { UserModule } from './module/user/user.module';
-import { ClassModule } from './module/class/class.module';
-import { AuthModule } from './module/auth/auth.module';
-import { AdminModule } from './module/admin/admin.module';
+import { UserModule } from './feature/user/user.module';
+import { ClassModule } from './feature/class/class.module';
+import { AuthModule } from './feature/auth/auth.module';
+import { AdminModule } from './feature/admin/admin.module';
 import dbConfig from './config/db.config';
 
 @Module({
