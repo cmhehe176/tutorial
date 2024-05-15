0f104595f8bda9bc2a0fb6a784215e62f9156172
Cong Minh <ndmc.system176@gmail.com>
2024-05-15 15:39:29 +0700

    update

--- nestjs/src/feature/auth/auth.service.ts
+++ nestjs/src/feature/auth/auth.service.ts
@@ -1,28 +1,21 @@
-import {
-  HttpException,
-  HttpStatus,
-  Injectable,
-  NotFoundException,
-} from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
 import { Login } from './auth.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AdminEntity } from 'src/database/entities/admin.entity';
 import { Repository } from 'typeorm';
-import { EMAIL_NOT_FOUND, USER_NOT_FOUND } from 'src/common/error';
+import { USER_NOT_FOUND } from 'src/common/error';
 
 @Injectable()
 export class AuthService {
-  constructor(
-    @InjectRepository(AdminEntity)
-    private admin_db: Repository<AdminEntity>,
-  ) {}
-  login = async (data: Login) => {
-    const email = await this.admin_db.findOneBy({ email: data.email });
+	constructor(
+		@InjectRepository(AdminEntity)
+		private admin_db: Repository<AdminEntity>,
+	) { }
+	login = async (data: Login) => {
+		const user = await this.admin_db.findOneBy({ email: data.email });
 
-    if (!email)
-      throw new HttpException(
-        { message: USER_NOT_FOUND },
-        HttpStatus.UNAUTHORIZED,
-      );
-  };
+		// if (user){throw new HttpException({message: USER_NOT_FOUND},HttpStatus.UNAUTHORIZED)}
+
+
+	};
 }
